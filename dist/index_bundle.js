6c04e96d470f46432266f5838d5d9a1d62b25ef2
Case104 <jpcase104@gmail.com>
2016-11-04 12:30:49 -0400

    Setup Dev Enviornment

---
+++ dist/index_bundle.js
@@ -0,0 +1,1 @@
+!function(e){function t(o){if(n[o])return n[o].exports;var r=n[o]={exports:{},id:o,loaded:!1};return e[o].call(r.exports,r,r.exports,t),r.loaded=!0,r.exports}var n={};return t.m=e,t.c=n,t.p="",t(0)}([function(e,t,n){e.exports=n(1)},function(e,t){const n=document.getElementById("app");n.innerHTML="hello"}]);
